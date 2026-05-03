71a314dc791e8b28ef98e86ac440794d63718392
razamustafa <razamustafa1383@gmail.com>
2026-05-04 00:37:16 +0530

    Intial commit

---
+++ src/main/java/io/lld_practice/LldPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.lld_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lld_practice/LldPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lld_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
